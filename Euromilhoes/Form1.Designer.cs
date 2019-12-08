aafc1a658b328cc476bc81099ac496fa183d7551
Paulo Coto <pbarca@hotmail.com>
2019-12-08 12:29:25 +0000

    ico

--- Euromilhoes/Form1.Designer.cs
+++ Euromilhoes/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -1106,9 +1107,10 @@
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Euromilhões";
+            this.Text = "euromilhões";
             this.ResumeLayout(false);
             this.PerformLayout();
 
