c3cdb103176e4f6a13e9cfed7ca3eff33e706938
Paulo Coto <pbarca@hotmail.com>
2019-12-16 19:52:24 +0000

    final

--- Euromilhoes/Form1.cs
+++ Euromilhoes/Form1.cs
@@ -14,26 +14,31 @@ namespace Euromilhoes
         private void estrela(object sender, EventArgs e)
         {
             Label tmp = (Label)sender;
-            tmp.Text = E.estrela(Convert.ToInt32(tmp.Tag)-1);
+            tmp.Text = E.estrela(Convert.ToInt32(tmp.Tag) - 1);
         }
 
         private void numero(object sender, EventArgs e)
         {
             Label tmp = (Label)sender;
-            tmp.Text = E.numero(Convert.ToInt32(tmp.Tag)-1);
+            tmp.Text = E.numero(Convert.ToInt32(tmp.Tag) - 1);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] ns = E.nsorteio;
+            int[] ns, es;
+            ns = E.nsorteio.ToArray();
+            es = E.esorteio.ToArray();
             n1.Text = ns[0].ToString();
             n2.Text = ns[1].ToString();
             n3.Text = ns[2].ToString();
             n4.Text = ns[3].ToString();
             n5.Text = ns[4].ToString();
-            int[] es = E.esorteio;
             e1.Text = es[0].ToString();
             e2.Text = es[1].ToString();
+            string frase = "Parabéns! Acertou em ";
+            frase += E.ncertos.ToString() + " números e ";
+            frase += E.ecertas.ToString() + " estrelas.";
+            MessageBox.Show(frase);
         }
 
     }
--- Euromilhoes/euromilhoes.cs
+++ Euromilhoes/euromilhoes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Euromilhoes
 {
@@ -9,7 +10,7 @@ namespace Euromilhoes
         private const string cruz = "✗";
         private bool[] numeros = new bool[50];
         private bool[] estrelas = new bool[12];
-        private int[] sortear(int q, int max)
+        private List<int> sortear(int q, int max)
         {
             Random rnd = new Random();
             List<int> s = new List<int>();
@@ -21,70 +22,42 @@ namespace Euromilhoes
                 s.Add(tmp);
             }
             s.Sort();
-            return s.ToArray();
+            return s;
         }
-        public int[] nsorteio => sortear(5, 50);
-        public int[] esorteio => sortear(2, 12);
-
+        private List<int> es, ns;
+        public List<int> nsorteio => ns = sortear(5, 50);
+        public List<int> esorteio => es = sortear(2, 12);
+        public int ncertos
+        {
+            get
+            {
+                int ncertos = 0;
+                for (int i = 0; i < 50; i++)
+                    if (numeros[i] && ns.Contains(i + 1)) ncertos++;
+                return ncertos;
+            }
+        }
+        public int ecertas
+        {
+            get
+            {
+                int ecertas = 0;
+                for (int i = 0; i < 12; i++) 
+                    if (estrelas[i] && es.Contains(i+1)) ecertas++;
+                return ecertas;
+            }
+        }
         public string numero(int n)
         {
-            if (numeros[n])
-            {
-                numeros[n] = false;
-                nnumero--;
-            }
-            else if (nnumero < 5)
-            {
-                numeros[n] = true;
-                nnumero++;
-            }
-
-            if (numeros[n]) return cruz;
-            else return "";
+            if (numeros[n]) { numeros[n] = false; nnumero--; }
+            else if (nnumero < 5) { numeros[n] = true; nnumero++; }
+            if (numeros[n]) return cruz; else return "";
         }
         public string estrela(int n)
         {
-            if (estrelas[n])
-            {
-                estrelas[n] = false;
-                nestrelas--;
-            }
-            else if (nestrelas < 2)
-            {
-                estrelas[n] = true;
-                nestrelas++;
-            }
-
-            if (estrelas[n]) return cruz;
-            else return "";
-        }
-        public string listanum
-        {
-            get
-            {
-                string lista = "";
-                for (int i = 0; i < 50; i++)
-                    if (numeros[i])
-                    {
-                        if (lista != "") lista += "-";
-                        lista += (i + 1).ToString();
-                    }
-                return lista;
-            }
-        }
-        public string listaestrelas
-        {
-            get
-            {
-                string lista = "";
-                for (int i = 0; i < 12; i++)
-                    if (estrelas[i])
-                    {
-                        if (lista != "") lista += "-";
-                        lista += (i + 1).ToString();
-                    }
-                return lista;
-            }
+            if (estrelas[n]) { estrelas[n] = false; nestrelas--; }
+            else if (nestrelas < 2) { estrelas[n] = true; nestrelas++; }
+            if (estrelas[n]) return cruz; else return "";
         }
     }
 }
